93df34690b28a496a7ffcedd5aa70baa8c244707
4iwen <luky.bien@gmail.com>
2022-10-05 18:52:11 +0200

    Initial commit

---
+++ FlexibleArray/FlexibleArray.cs
@@ -0,0 +1,116 @@
+﻿namespace FlexibleArray;
+
+public class FlexibleArray<T>
+{
+    private T[] _data;
+
+    public int Length => _data.Length;
+
+    public FlexibleArray(T[] data)
+    {
+        _data = data;
+    }
+    
+    public FlexibleArray(int size)
+    {
+        _data = new T[size];
+    }
+
+    public void InsertAt(int index, T value)
+    {
+        if (index > _data.Length)
+            throw new IndexOutOfRangeException("Index greater than array length");
+        
+        T[] temp = new T[_data.Length + 1];
+        
+        for (int i = 0; i < index; i++)
+        {
+            temp[i] = _data[i];
+        }
+        
+        temp[index] = value;
+        
+        for (int i = index + 1; i < temp.Length; i++)
+        {
+            temp[i] = _data[i - 1];
+        }
+        
+        _data = temp;
+    }
+
+    public void RemoveAt(int index)
+    {
+        if (index >= _data.Length)
+            throw new IndexOutOfRangeException("Index greater than array length");
+        
+        T[] temp = new T[_data.Length - 1];
+        
+        for (int i = 0; i < index; i++)
+        {
+            temp[i] = _data[i];
+        }
+
+        for (int i = index; i < _data.Length - 1; i++)
+        {
+            temp[i] = _data[i + 1];
+        }
+        
+        _data = temp;
+    }
+    
+    public void Join(FlexibleArray<T> other)
+    {
+        T[] temp = new T[_data.Length + other._data.Length];
+        
+        uint i = 0;
+        uint j = 0;
+        uint k = 0;
+        
+        while (i < _data.Length && j < other._data.Length)
+        {
+            temp[k++] = _data[i++];
+            temp[k++] = other._data[j++];
+        }
+        
+        while (i < _data.Length)
+        {
+            temp[k++] = _data[i++];
+        }
+        
+        while (j < other._data.Length)
+        {
+            temp[k++] = other._data[j++];
+        }
+        
+        _data = temp;
+    }
+
+    public void SplitByOddEvenIndex(out FlexibleArray<T> even, out FlexibleArray<T> odd)
+    {
+        even = new FlexibleArray<T>(_data.Length / 2 + _data.Length % 2);
+        odd = new FlexibleArray<T>(_data.Length / 2);
+
+        for (int i = 0; i < _data.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                even[i / 2] = _data[i];
+            }
+            else
+            {
+                odd[i / 2] = _data[i];
+            }
+        }
+    }
+    
+    public T this[int index]
+    {
+        get => _data[index];
+        set => _data[index] = value;
+    }
+
+    public override string ToString()
+    {
+        return $"Type: [{typeof(T)}],\nLength: [{_data.Length}],\nData: [{String.Join(", ", _data)}]\n";
+    }
+}
---
+++ FlexibleArray/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace FlexibleArray;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Example();
+    }
+    
+    private static void Example()
+    {
+        Random random = new Random();
+
+        FlexibleArray<int> array = new FlexibleArray<int>(random.Next(1, 30));
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(100);
+        }
+
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("- Insert and remove elements -");
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine(array.ToString());
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Insert at");
+        Console.ForegroundColor = ConsoleColor.Green;
+        array.InsertAt(2, 333);
+        Console.WriteLine(array.ToString());
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Remove at");
+        Console.ForegroundColor = ConsoleColor.Green;
+        array.RemoveAt(2);
+        Console.WriteLine(array.ToString());
+        
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("- Joining two arrays -");
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine(array.ToString());
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Join");
+        Console.ForegroundColor = ConsoleColor.Green;
+        array.Join(new FlexibleArray<int>(new [] { 1, 2, 3, 4, 5, }));
+        Console.WriteLine(array.ToString());
+        
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("- Splitting array by odd and even indices -");
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine(array.ToString());
+        FlexibleArray<int> odd;
+        FlexibleArray<int> even;
+        array.SplitByOddEvenIndex(out even, out odd);
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Even");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(even.ToString());
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Odd");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(odd.ToString());
+    }
+}
